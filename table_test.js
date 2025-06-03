cce8b2ca21b2e6426f9141afc96978ea3d3de06b
nickg <217179+client9@users.noreply.github.com>
2025-06-02 18:32:07 -0700

    add 1-d Table function

---
+++ table_test.js
@@ -0,0 +1,12 @@
+import test from "node:test";
+import { strict as assert } from "node:assert";
+
+import { Table } from "./list.js";
+
+test("Table", (t) => {
+    assert.deepEqual( Table(1, 3), [1,1,1])
+    assert.deepEqual( Table(x => x, [3]), [0,1,2])
+    assert.deepEqual( Table(x => x, [1,4]), [1,2,3])
+    assert.deepEqual( Table(x => x, [1,6,2]), [1,3,5])
+
+})
