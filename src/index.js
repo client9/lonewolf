ee0bf364d030865ad44a77035dcb57d29eed786a
Nick Galbreath <client9@users.noreply.github.com>
2025-06-08 17:32:15 +0100

    additions, cleanups

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
-export * from "./dispatch.js";
+import { Define, Call, Eval } from "./dispatch.js";
 
-export * from "./Equal.js";
-export * from "./AllTrue.js";
-export * from "./ArrayDepth.js";
-export * from "./Dimensions.js";
-export * from "./StringRepeat.js";
-export * from "./ListQ.js";
-export * from "./MapList.js";
-export * from "./Range.js";
-export * from "./Reverse.js";
-export * from "./StringPadLeft.js";
 import {
   Repeated,
   Pattern,
   MatchAnd,
   MatchAny,
+  MatchExpr,
   MatchOr,
+  MatchOne,
   MatchType,
   MatchHead,
   MatchNone,
+  MatchEqual,
 } from "./pattern.js";
+
+import AllTrue from "./AllTrue.js";
 import Apply from "./Apply.js";
-import ArrayPlot from "./ArrayPlot";
+import ArrayDepth from "./ArrayDepth.js";
+import ArrayPlot from "./ArrayPlot.js";
 import AnyTrue from "./AnyTrue.js";
 import BitXor from "./BitXor.js";
 import BitAnd from "./BitAnd.js";
 import BitOr from "./BitOr.js";
+import Circle from "./Circle.js";
+import Dimensions from "./Dimensions.js";
 import Do from "./Do.js";
+import Dot from "./Dot.js";
+import Equal from "./Equal.js";
 import Grayscale from "./Grayscale.js";
 import HTMLTag from "./HTMLTag.js";
-import NoneTrue from "./NoneTrue.js";
+import ListQ from "./ListQ.js";
+import MapList from "./MapList.js";
 import MapThread from "./MapThread.js";
 import MapIndexed from "./MapIndexed.js";
+import MatrixQ from "./MatrixQ.js";
 import Mean from "./Mean.js";
+import NoneTrue from "./NoneTrue.js";
 import Expr from "./Expr.js";
 import Head from "./Head.js";
 import First from "./First.js";
@@ -41,41 +44,63 @@ import Most from "./Most.js";
 import Partition from "./Partition.js";
 import Plus from "./Plus.js";
 import RandomInteger from "./RandomInteger.js";
+import Range from "./Range.js";
 import Rest from "./Rest.js";
+import Reverse from "./Reverse.js";
 import Table from "./Table.js";
 import Times from "./Times.js";
 import Timing from "./Timing.js";
 import ToString from "./ToString.js";
+import ToSVG from "./ToSVG.js";
 import Transpose from "./Transpose.js";
 import StringJoin from "./StringJoin.js";
+import StringRepeat from "./StringRepeat.js";
 import StringRiffle from "./StringRiffle.js";
 import StringPadLeft from "./StringPadLeft.js";
 import SameQ from "./SameQ.js";
 import RGBColor from "./RGBColor.js";
 import CSSColor from "./CSSColor.js";
 import Uint64 from "./Uint64.js";
+import VectorQ from "./VectorQ.js";
 
 export {
+  AllTrue,
   AnyTrue,
   Apply,
+  ArrayDepth,
   ArrayPlot,
-  CSSColor,
+  BitAnd,
+  BitOr,
   BitXor,
+  Call,
+  Circle,
+  CSSColor,
+  Define,
+  Dimensions,
   Do,
+  Dot,
+  Equal,
   Expr,
+  Eval,
   First,
   Grayscale,
   Head,
   HTMLTag,
   Last,
+  ListQ,
   MapIndexed,
+  MapList,
   MapThread,
   MatchType,
   MatchHead,
   MatchAnd,
   MatchAny,
+  MatchEqual,
+  MatchExpr,
+  MatchOne,
   MatchOr,
   MatchNone,
+  MatrixQ,
   Mean,
   Most,
   NoneTrue,
@@ -83,16 +108,21 @@ export {
   Pattern,
   Plus,
   RandomInteger,
+  Range,
   Repeated,
+  Reverse,
   RGBColor,
   Rest,
   SameQ,
   StringJoin,
+  StringRepeat,
   StringRiffle,
   StringPadLeft,
   Table,
   Timing,
   ToString,
+  ToSVG,
   Transpose,
   Uint64,
+  VectorQ,
 };
