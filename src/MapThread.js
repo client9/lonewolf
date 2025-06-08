2dfaac25da575318e799f9ee9d9349dd7d47bf28
Nick Galbreath <client9@users.noreply.github.com>
2025-06-08 17:20:37 +0100

    make args standardized

--- src/MapThread.js
+++ src/MapThread.js
@@ -2,6 +2,6 @@ import Transpose from "./Transpose.js";
 import MapList from "./MapList.js";
 import Apply from "./Apply.js";
 
-export default function MapThread(fn, ...lists) {
+export default function MapThread(fn, lists) {
   return MapList((x) => Apply(fn, x), Transpose(lists));
 }
